53fbec0ca69e3c7029f82ceb9854e51bbdffe076
Abhishek Girish <agirish@mapr.com>
2015-09-14 15:26:49 -0700

    Updating scripts to remove hardcoded arguments

--- framework/resources/Datasources/ctas_auto_partition/setup.sh
+++ framework/resources/Datasources/ctas_auto_partition/setup.sh
@@ -4,7 +4,7 @@ hadoop fs -mkdir /drill/testdata/ctas_auto_partition/tpch_single_partition
 hadoop fs -rmr /drill/testdata/ctas_auto_partition/tpch_single_partition1
 hadoop fs -mkdir /drill/testdata/ctas_auto_partition/tpch_single_partition1
 
-${DRILL_HOME}/bin/sqlline -n user1 -p mapr -u "jdbc:drill:schema=dfs.ctasAutoPartition;drillbit=${DRILL_STORAGE_PLUGIN_SERVER}"  --run=resources/Datasources/ctas_auto_partition/create_tables.ddl
+${DRILL_HOME}/bin/sqlline -n user1 -p ${PASSWORD} -u "jdbc:drill:schema=dfs.ctasAutoPartition;drillbit=${DRILL_STORAGE_PLUGIN_SERVER}"  --run=resources/Datasources/ctas_auto_partition/create_tables.ddl
 
 # it seems that sqlline does not exit, if one of the queries failed.
 if [ "$?" -eq 0 ]
--- framework/resources/Datasources/tpcds/createTablesJson.sh
+++ framework/resources/Datasources/tpcds/createTablesJson.sh
@@ -2,5 +2,5 @@
 
 hadoop fs -test -d /drill/testdata/tpcds_sf1/json ;if [ `echo $?` -eq 0 ]; then hadoop fs -rmr /drill/testdata/tpcds_sf1/json; hadoop fs -mkdir /drill/testdata/tpcds_sf1/json; else hadoop fs -mkdir /drill/testdata/tpcds_sf1/json; fi
 
-${DRILL_HOME}/bin/sqlline -n root -p mapr -u "jdbc:drill:schema=dfs.tpcds_sf1_json;drillbit=${DRILL_STORAGE_PLUGIN_SERVER}"  --run=resources/Datasources/tpcds/createTablesJson.sql
+${DRILL_HOME}/bin/sqlline -n root -p ${PASSWORD} -u "jdbc:drill:schema=dfs.tpcds_sf1_json;drillbit=${DRILL_STORAGE_PLUGIN_SERVER}"  --run=resources/Datasources/tpcds/createTablesJson.sql
 resources/Datasources/tpcds/createViewsJson.sh
