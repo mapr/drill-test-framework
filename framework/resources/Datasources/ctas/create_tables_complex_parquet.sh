8585022308a29b58940106a94cbfac784786ffe1
Abhishek Girish <agirish@mapr.com>
2015-09-15 11:39:14 -0700

    Updating scripts to remove hardcoded arguments - 2

--- framework/resources/Datasources/ctas/create_tables_complex_parquet.sh
+++ framework/resources/Datasources/ctas/create_tables_complex_parquet.sh
@@ -12,7 +12,7 @@
 hadoop fs -mkdir /drill/testdata/complex/parquet
 hadoop fs -chmod 777 /drill/testdata/complex/parquet
 
-${DRILL_HOME}/bin/sqlline -n user1 -p ${PASSWORD} -u "jdbc:drill:schema=dfs.drillTestDirComplexParquet;drillbit=${DRILL_STORAGE_PLUGIN_SERVER}"  --run=resources/Datasources/ctas/create_tables_complex_parquet.ddl
+${DRILL_HOME}/bin/sqlline -n ${USERNAME} -p ${PASSWORD} -u "jdbc:drill:schema=dfs.drillTestDirComplexParquet;drillbit=${DRILL_STORAGE_PLUGIN_SERVER}"  --run=resources/Datasources/ctas/create_tables_complex_parquet.ddl
 
 # TODO: it seems that sqlline does not exit, if one of the queries failed.
 if [ "$?" -eq 0 ]
--- framework/resources/Datasources/tpcds/createViewsParquet.sh
+++ framework/resources/Datasources/tpcds/createViewsParquet.sh
@@ -2,4 +2,4 @@
 
 hadoop fs -test -d /drill/testdata/tpcds_sf1/parquet/views ;if [ `echo $?` -eq 1 ]; then hadoop fs -mkdir /drill/testdata/tpcds_sf1/parquet/views; fi
 
-${DRILL_HOME}/bin/sqlline -n root -p ${PASSWORD} -u "jdbc:drill:schema=dfs.tpcds_sf1_parquet_views;drillbit=${DRILL_STORAGE_PLUGIN_SERVER}"  --run=resources/Datasources/tpcds/createViewsParquet.sql
+${DRILL_HOME}/bin/sqlline -n ${USERNAME} -p ${PASSWORD} -u "jdbc:drill:schema=dfs.tpcds_sf1_parquet_views;drillbit=${DRILL_STORAGE_PLUGIN_SERVER}"  --run=resources/Datasources/tpcds/createViewsParquet.sql
