fb6ad5191bc37d4d1a9dda52a316f2eff5109e04
Abhishek Girish <agirish@mapr.com>
2015-09-15 11:59:31 -0700

    Correcting a small typo

--- framework/resources/Datasources/ctas_auto_partition/ctas_existing_partition_pruning.sh
+++ framework/resources/Datasources/ctas_auto_partition/ctas_existing_partition_pruning.sh
@@ -2,7 +2,7 @@ hadoop fs -mkdir /drill/testdata/ctas_auto_partition
 hadoop fs -rmr /drill/testdata/ctas_auto_partition/existing_partition_pruning
 hadoop fs -mkdir /drill/testdata/ctas_auto_partition/existing_partition_pruning
 
-${DRILL_HOME}/bin/sqlline -n $(USERNAME} -p ${PASSWORD} -u "jdbc:drill:schema=dfs.ctasAutoPartition;drillbit=${DRILL_STORAGE_PLUGIN_SERVER}"  --run=resources/Datasources/ctas_auto_partition/ctas_existing_partition_pruning.ddl
+${DRILL_HOME}/bin/sqlline -n ${USERNAME} -p ${PASSWORD} -u "jdbc:drill:schema=dfs.ctasAutoPartition;drillbit=${DRILL_STORAGE_PLUGIN_SERVER}"  --run=resources/Datasources/ctas_auto_partition/ctas_existing_partition_pruning.ddl
 
 # it seems that sqlline does not exit, if one of the queries failed.
 if [ "$?" -eq 0 ]
