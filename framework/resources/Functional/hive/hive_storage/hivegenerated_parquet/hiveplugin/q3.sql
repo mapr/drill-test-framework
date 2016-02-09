cc8a598223f834fa57ca7ecd0deb348261753441
Rahul Challapalli <challapallirahul@gmail.com>
2016-02-09 23:06:39 +0000

    Modified the queries and baselines to get rid of the timestamp column which varies depending on the cluster where the test is run

--- framework/resources/Functional/hive/hive_storage/hivegenerated_parquet/hiveplugin/q3.sql
+++ framework/resources/Functional/hive/hive_storage/hivegenerated_parquet/hiveplugin/q3.sql
@@ -2,7 +2,6 @@ select
   sum(int_col), 
   max(bigint_col), 
   min(time_col), 
-  min(timestamp_col), 
   min(date_sub(date_col, 365)), 
   min(length(varchar_col)), 
   avg(float_col/double_col), 
