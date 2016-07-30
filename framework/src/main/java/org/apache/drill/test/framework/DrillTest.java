8a5db430eda8a353c33e235f5d61942d41f5210e
Prasad Subramanya <psubramanya@PSUBRAMANYA-E365-MPR13.local>
2016-07-29 18:09:28 -0700

    Adding support for json reports

--- framework/src/main/java/org/apache/drill/test/framework/DrillTest.java
+++ framework/src/main/java/org/apache/drill/test/framework/DrillTest.java
@@ -17,6 +17,7 @@
  */
 package org.apache.drill.test.framework;
 
+import com.google.common.base.Stopwatch;
 import org.apache.drill.test.framework.TestVerifier.TestStatus;
 
 public interface DrillTest extends Cancelable {
@@ -24,4 +25,7 @@ public interface DrillTest extends Cancelable {
   public Exception getException();
   public String getInputFile();
   public String getQuery();
+  public String getTestId();
+  public int getCloneId();
+  public Stopwatch getDuration();
 }
