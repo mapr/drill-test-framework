cea111553a14caddfed474eedc2a83228f41f9e8
cchung <cchang@maprtech.com>
2017-06-16 15:52:48 -0700

    MD-2094: reset system options to default inside before-run.sql

--- before-run.sql
+++ before-run.sql
@@ -1,5 +1,6 @@
 select * from sys.version;
 select * from sys.drillbits;
+alter system reset all;
 alter system set `planner.enable_decimal_data_type` = true;
 alter system set `new_view_default_permissions` = '777';
 alter system set `planner.enable_limit0_optimization` = true;
