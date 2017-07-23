4475850d6c97f3879e1c3860eec7f0bf0b7064ef
Abhishek Girish <agirish@apache.org>
2017-07-23 07:56:49 -0700

    Increase planner.memory.max_query_memory_per_node (#298)
    The current default of 2 GB is now insufficient, as the 2 GB limit is being used by both Hash Agg and Managed Sort. As a result multiple queries fail with OOM.
    
    Temporarily changing this limit to a much higher value (10 GB), until the default is updated suitably (tracked by DRILL-3549).

--- after-run.sql
+++ after-run.sql
@@ -1,5 +1,3 @@
-alter system set `planner.enable_decimal_data_type` = false;
-alter system set `new_view_default_permissions` = '700';
-alter system set `planner.enable_limit0_optimization` = false;
-alter system set `exec.errors.verbose` = false;
+select * from sys.version;
 select * from sys.drillbits;
+alter system reset all;
--- before-run.sql
+++ before-run.sql
@@ -5,3 +5,4 @@ alter system set `planner.enable_decimal_data_type` = true;
 alter system set `new_view_default_permissions` = '777';
 alter system set `planner.enable_limit0_optimization` = true;
 alter system set `exec.errors.verbose` = true;
+alter system set `planner.memory.max_query_memory_per_node` = 10737418240;
