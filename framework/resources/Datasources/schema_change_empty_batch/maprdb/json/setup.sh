a638cd11b931b5ad7f8aeffea95b67c6d7c92f30
Prasad Subramanya <prasadsubramanya@psubram-1010.local>
2017-09-12 16:01:32 -0700

    Providing read permission to maprdb json tables

--- framework/resources/Datasources/schema_change_empty_batch/maprdb/json/setup.sh
+++ framework/resources/Datasources/schema_change_empty_batch/maprdb/json/setup.sh
@@ -20,3 +20,7 @@ java -cp ${HADOOP_INSTALL_LOC}/lib/*:${HADOOP_INSTALL_LOC}/hadoop/hadoop-2.7.0/s
 mapr importJSON -src ${DRILL_TESTDATA}/schema_change_empty_batch/maprdb/json/part.json -dst ${DRILL_TESTDATA}/schema_change_empty_batch/maprdb/json/part -idfield row_key -bulkload true -mapreduce true
 
 mapr importJSON -src ${DRILL_TESTDATA}/schema_change_empty_batch/maprdb/json/partsupp.json -dst ${DRILL_TESTDATA}/schema_change_empty_batch/maprdb/json/partsupp -idfield row_key -bulkload true -mapreduce true
+
+maprcli table cf edit -path ${DRILL_TESTDATA}/schema_change_empty_batch/maprdb/json/part -cfname default -readperm 'u:'"$USER"'|u:mapr'
+
+maprcli table cf edit -path ${DRILL_TESTDATA}/schema_change_empty_batch/maprdb/json/partsupp -cfname default -readperm 'u:'"$USER"'|u:mapr'
