df86b3dbd79cec8eeb109030619ab2cbc6e32710
Prasad Subramanya <prasadsubramanya@psubram-1010.local>
2017-09-15 16:57:24 -0700

    MD-734: Removed multiple copies of execHive.sh

--- framework/resources/Datasources/hive_storage/hbase/main.sh
+++ framework/resources/Datasources/hive_storage/hbase/main.sh
@@ -8,6 +8,6 @@ ${DRILL_TEST_DATA_DIR}/Datasources/hive_storage/hbase/hbase.sh
 
 wait
 
-${DRILL_TEST_DATA_DIR}/bin/execHive.sh ${DRILL_TEST_DATA_DIR}/Datasources/hive_storage/hive_hbase.q
+${DRILL_TEST_DATA_DIR}/Datasources/hive/execHive.sh ${DRILL_TEST_DATA_DIR}/Datasources/hive_storage/hive_hbase.q
 
 set +x
