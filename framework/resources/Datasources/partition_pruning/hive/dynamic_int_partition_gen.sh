e6e04b2ff4bfab819e0b380fb8b28ef98f6ef0ef
Prasad Nagaraj Subramanya <prasadns14@gmail.com>
2017-09-18 10:56:44 -0700

    MD-734: Remove multiple copies of execHive.sh (#322)

--- framework/resources/Datasources/partition_pruning/hive/dynamic_int_partition_gen.sh
+++ framework/resources/Datasources/partition_pruning/hive/dynamic_int_partition_gen.sh
@@ -1,2 +1,2 @@
-framework/resources/Datasources/hive_storage/execHive.sh framework/resources/Datasources/partition_pruning/hive/hive.ddl
-framework/resources/Datasources/hive_storage/execHive.sh framework/resources/Datasources/partition_pruning/hive/hive_int_parquet.ddl
+framework/resources/Datasources/hive/execHive.sh framework/resources/Datasources/partition_pruning/hive/hive.ddl
+framework/resources/Datasources/hive/execHive.sh framework/resources/Datasources/partition_pruning/hive/hive_int_parquet.ddl
