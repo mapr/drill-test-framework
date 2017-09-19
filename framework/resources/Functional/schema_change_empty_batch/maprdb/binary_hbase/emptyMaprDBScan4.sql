500cba9a0434287d271a2b6c72d0f15a48b9edde
Prasad Subramanya <prasadsubramanya@psubram-1010.local>
2017-09-19 10:59:55 -0700

    MD-2296: New cases to run MapRDB binary cases with hbase storage plugin

---
+++ framework/resources/Functional/schema_change_empty_batch/maprdb/binary_hbase/emptyMaprDBScan4.sql
@@ -0,0 +1,1 @@
+select * from `/drill/testdata/schema_change_empty_batch/maprdb/binary/empty`;
---
+++ framework/resources/Functional/schema_change_empty_batch/maprdb/binary_hbase/emptyMaprDBScan6.sql
@@ -0,0 +1,1 @@
+select DISTINCT t.data.col from `/drill/testdata/schema_change_empty_batch/maprdb/binary/empty` as t;
