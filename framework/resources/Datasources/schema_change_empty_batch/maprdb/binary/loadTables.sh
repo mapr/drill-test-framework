2470f5b504924c688ec961f7d0d79b8dcbe32a06
Prasad Nagaraj Subramanya <prasadns14@gmail.com>
2017-10-18 15:41:18 -0700

    MD-2961: Use hbase importTsv

--- framework/resources/Datasources/schema_change_empty_batch/maprdb/binary/loadTables.sh
+++ framework/resources/Datasources/schema_change_empty_batch/maprdb/binary/loadTables.sh
@@ -4,5 +4,5 @@ source conf/drillTestConfig.properties
 set -e
 set -x
 
-hbase org.apache.hadoop.hbase.mapreduce.Import ${DRILL_TESTDATA}/schema_change_empty_batch/maprdb/binary/part ${DRILL_TESTDATA}/schema_change_empty_batch/maprdb/binary/data/part/
-hbase org.apache.hadoop.hbase.mapreduce.Import ${DRILL_TESTDATA}/schema_change_empty_batch/maprdb/binary/partsupp ${DRILL_TESTDATA}/schema_change_empty_batch/maprdb/binary/data/partsupp/
+hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dmapred.reduce.tasks.speculative.execution=false -Dimporttsv.columns=HBASE_ROW_KEY,data:p_partkey,data:p_name,data:p_mfgr,data:p_brand,data:p_type,data:p_size,data:p_container,data:p_retailprice,data:p_comment ${DRILL_TESTDATA}/schema_change_empty_batch/maprdb/binary/part ${DRILL_TESTDATA}/schema_change_empty_batch/maprdb/binary/data/part.tsv
+hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dmapred.map.tasks.speculative.execution=false -Dimporttsv.columns=HBASE_ROW_KEY,data:ps_partkey,data:ps_suppkey,data:ps_availqty,data:ps_supplycost,data:ps_comment ${DRILL_TESTDATA}/schema_change_empty_batch/maprdb/binary/partsupp ${DRILL_TESTDATA}/schema_change_empty_batch/maprdb/binary/data/partsupp.tsv
