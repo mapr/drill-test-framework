d0c54ec5b2b3f6385ed0b52e30cab2cc2a6383a6
Prasad Nagaraj Subramanya <prasadns14@gmail.com>
2017-10-19 11:29:45 -0700

    MD-2961: Use hbase importTsv (#344)

--- framework/resources/Datasources/schema_change_empty_batch/maprdb/binary/copyFiles.sh
+++ framework/resources/Datasources/schema_change_empty_batch/maprdb/binary/copyFiles.sh
@@ -8,7 +8,7 @@ if ! $(hadoop fs -test -d ${DRILL_TESTDATA}/schema_change_empty_batch/maprdb/bin
     then
         hadoop fs -mkdir -p ${DRILL_TESTDATA}/schema_change_empty_batch/maprdb/binary/data
 
-        hadoop fs -put ${DRILL_TEST_DATA_DIR}/Datasources/schema_change_empty_batch/data/hbase/* ${DRILL_TESTDATA}/schema_change_empty_batch/maprdb/binary/data/
+        hadoop fs -put ${DRILL_TEST_DATA_DIR}/Datasources/schema_change_empty_batch/data/tsv/* ${DRILL_TESTDATA}/schema_change_empty_batch/maprdb/binary/data/
 fi
 
 set +x
