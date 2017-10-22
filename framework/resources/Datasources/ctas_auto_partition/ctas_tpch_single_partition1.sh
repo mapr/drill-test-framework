025a550fc303a6b268e18745e98184ee0663ad0b
Prasad Nagaraj Subramanya <prasadns14@gmail.com>
2017-10-21 17:59:24 -0700

    MD-3028: Adding username parameter for sqlline connect (#346)

--- framework/resources/Datasources/ctas_auto_partition/ctas_tpch_single_partition1.sh
+++ framework/resources/Datasources/ctas_auto_partition/ctas_tpch_single_partition1.sh
@@ -7,7 +7,7 @@ hadoop fs -mkdir /drill/testdata/ctas_auto_partition/tpch_single_partition1
 
 if [ -z "$PASSWORD" ]
 then
-  ${DRILL_HOME}/bin/sqlline -u "jdbc:drill:schema=dfs.ctasAutoPartition;drillbit=${DRILL_STORAGE_PLUGIN_SERVER}"  --run=${DRILL_TEST_DATA_DIR}/Datasources/ctas_auto_partition/ctas_tpch_single_partition1.ddl
+  ${DRILL_HOME}/bin/sqlline -n ${USERNAME} -u "jdbc:drill:schema=dfs.ctasAutoPartition;drillbit=${DRILL_STORAGE_PLUGIN_SERVER}" --run=${DRILL_TEST_DATA_DIR}/Datasources/ctas_auto_partition/ctas_tpch_single_partition1.ddl
 else
   ${DRILL_HOME}/bin/sqlline -n ${USERNAME} -p ${PASSWORD} -u "jdbc:drill:schema=dfs.ctasAutoPartition;drillbit=${DRILL_STORAGE_PLUGIN_SERVER}"  --run=${DRILL_TEST_DATA_DIR}/Datasources/ctas_auto_partition/ctas_tpch_single_partition1.ddl
 fi
--- framework/resources/Datasources/limit0/aggregates/aggregation/scalar/create_scalar_views.sh
+++ framework/resources/Datasources/limit0/aggregates/aggregation/scalar/create_scalar_views.sh
@@ -6,7 +6,7 @@ set -e
 
 if [ -z "$PASSWORD" ]
 then
-  ${DRILL_HOME}/bin/sqlline -u "jdbc:drill:schema=dfs.aggregation;drillbit=${DRILL_STORAGE_PLUGIN_SERVER}"  --run=${DRILL_TEST_DATA_DIR}/Datasources/limit0/aggregates/aggregation/scalar/create_scalar_views.sql
+  ${DRILL_HOME}/bin/sqlline -n ${USERNAME} -u "jdbc:drill:schema=dfs.aggregation;drillbit=${DRILL_STORAGE_PLUGIN_SERVER}" --run=${DRILL_TEST_DATA_DIR}/Datasources/limit0/aggregates/aggregation/scalar/create_scalar_views.sql
 else
   ${DRILL_HOME}/bin/sqlline -n ${USERNAME} -p ${PASSWORD} -u "jdbc:drill:schema=dfs.aggregation;drillbit=${DRILL_STORAGE_PLUGIN_SERVER}"  --run=${DRILL_TEST_DATA_DIR}/Datasources/limit0/aggregates/aggregation/scalar/create_scalar_views.sql
 fi
