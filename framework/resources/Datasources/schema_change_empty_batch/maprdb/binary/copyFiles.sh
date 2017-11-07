61dfe15d577d348a189b273dd0d9f3b2dc75db8f
Abhishek Girish <agirish@apache.org>
2017-11-07 09:55:29 -0800

    Reverting a change in (#360)

--- framework/resources/Datasources/schema_change_empty_batch/maprdb/binary/copyFiles.sh
+++ framework/resources/Datasources/schema_change_empty_batch/maprdb/binary/copyFiles.sh
@@ -7,6 +7,6 @@ set -e
 hadoop fs -rm -r -f ${DRILL_TESTDATA}/schema_change_empty_batch/maprdb/binary/data
 hadoop fs -mkdir -p ${DRILL_TESTDATA}/schema_change_empty_batch/maprdb/binary/data
 
-hadoop fs -put -f ${DRILL_TEST_DATA_DIR}/Datasources/schema_change_empty_batch/data/psv/* ${DRILL_TESTDATA}/schema_change_empty_batch/maprdb/binary/data/
+hadoop fs -put -f ${DRILL_TEST_DATA_DIR}/Datasources/schema_change_empty_batch/data/tsv/* ${DRILL_TESTDATA}/schema_change_empty_batch/maprdb/binary/data/
 
 set +x
