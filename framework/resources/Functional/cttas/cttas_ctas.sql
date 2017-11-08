860063051bde39f275625306f3f51758d8161271
cchung <cchang@maprtech.com>
2017-11-07 17:06:43 -0800

    MD-3149: add drop table if exists

--- framework/resources/Functional/cttas/cttas_ctas.sql
+++ framework/resources/Functional/cttas/cttas_ctas.sql
@@ -1,5 +1,8 @@
+DROP TABLE IF EXISTS dfs.drillTestDir.t1_tmp;
+DROP TABLE IF EXISTS dfs.tmp.t1_tmp;
 CREATE TABLE dfs.drillTestDir.t1_tmp AS SELECT * FROM typeall_l;
 CREATE TEMPORARY TABLE dfs.tmp.t1_tmp AS SELECT * FROM typeall_l;
+--@test
 SELECT COUNT(*) FROM dfs.tmp.t1_tmp;
 DROP TABLE dfs.tmp.t1_tmp;
 DROP TABLE dfs.drillTestDir.t1_tmp;
