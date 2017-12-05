a1797f11869b7262270669305477267c692b9d3e
Prasad Nagaraj Subramanya <prasadns14@gmail.com>
2017-12-04 16:17:40 -0800

    MD-2372: Removed overlap of hive scripts (#372)
    * MD-2372: Removed lock mechanism in execHive.sh
    
    * MD-2372: Removed overlap of hive scripts
    
    * MD-2372: Removed overlap of hive scripts
    
    * MD-2372: Removed overlap of hive scripts
    
    * MD-2372: Removed overlap of hive scripts

--- framework/resources/Datasources/hive_storage/hive_functions_gen.sh
+++
@@ -1,5 +0,0 @@
-framework/resources/Datasources/hive/execHive.sh framework/resources/Datasources/hive_storage/hive_storage.ddl
-framework/resources/Datasources/hive/execHive.sh framework/resources/Datasources/hive_storage/sample.ddl
-framework/resources/Datasources/hive/execHive.sh framework/resources/Datasources/hive_storage/tpch0.01/tpch.ddl
-framework/resources/Datasources/cross-sources/cross-sources.sh
-framework/resources/Datasources/hive/execHive.sh framework/resources/Datasources/hive_storage/tpcds01_text.ddl
