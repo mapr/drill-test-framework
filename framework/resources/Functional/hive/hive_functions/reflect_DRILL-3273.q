b5282bd7c8d5d37a85555c48635e14a991cb5fb7
Denys Ordynskiy <denysord88@gmail.com>
2018-05-03 20:27:58 +0300

    MD-4256 Decimal data type enhancements

--- framework/resources/Functional/hive/hive_functions/reflect_DRILL-3273.q
+++ framework/resources/Functional/hive/hive_functions/reflect_DRILL-3273.q
@@ -3,6 +3,6 @@ SELECT reflect('java.lang.String', 'valueOf', float_col),
        reflect('java.lang.Math', 'max', 66, 23),
        reflect('java.lang.Math', 'min', 2, 3),
        reflect('java.lang.Math', 'round', float_col),
-       reflect('java.lang.Math', 'exp', 1.0),
+       reflect('java.lang.Math', 'exp', cast(1.0 as double)),
        reflect('java.lang.Math', 'floor', double_col)
 from hive.crosssources.fewtypes_null_hive;
--- framework/resources/Functional/schema_change_empty_batch/maprdb/json/emptyMaprDBJson_MD1115.sql
+++ framework/resources/Functional/schema_change_empty_batch/maprdb/json/emptyMaprDBJson_MD1115.sql
@@ -1,3 +1,3 @@
 set `planner.slice_target`=1;
-select t.ps_partkey, CAST(sum(t.ps_supplycost) as DECIMAL(4,2)) as numSuppliers from `maprdb/json/partsupp1` as t where t.ps_partkey > 3 AND t.ps_partkey < 6 group by t.ps_partkey;
+select t.ps_partkey, CAST(sum(t.ps_supplycost) as DECIMAL(6,2)) as numSuppliers from `maprdb/json/partsupp1` as t where t.ps_partkey > 3 AND t.ps_partkey < 6 group by t.ps_partkey;
 reset `planner.slice_target`;
