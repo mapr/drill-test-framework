9d260db390fbd97953549870855e725d9f7c6ac1
Anton Gozhiy <31588230+agozhiy@users.noreply.github.com>
2018-05-06 23:01:15 +0300

    DRILL-6259: Adding tests to verify filter pushdown (#458)

---
+++ framework/resources/Functional/filter/pushdown/plan_no_order/DRILL_6259_filter_push_down_nested_list_of_complex_fields_nested_array_null_values_plan.sql
@@ -0,0 +1,1 @@
+explain plan for select * from dfs.drillTestDir.`parquet_storage/DRILL_6259/DRILL_6259_test_data` t where t.complex_field.nested_list_of_complex_fields[1].nested_array[3] is null;
---
+++ framework/resources/Functional/filter/pushdown/plan_no_order/DRILL_6259_filter_push_down_nested_list_of_complex_fields_nested_field_null_values_plan.sql
@@ -0,0 +1,2 @@
+explain plan for select * from dfs.drillTestDir.`parquet_storage/DRILL_6259/DRILL_6259_test_data` t
+where not t.complex_field.nested_list_of_complex_fields[0].nested_field is null;
