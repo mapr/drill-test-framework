058c6a42fd0924bd0a5b7f727f25c41f1d2d2eb8
Kedar Sankar Behera <kedar.behera@gmail.com>
2018-05-31 19:25:39 -0700

    added flatten queries

---
+++ framework/resources/Functional/lateralUnnest/sf0dot01/04.sql
@@ -0,0 +1,1 @@
+select f.c_custkey, f.c_name, f.o.o_orderkey as o_orderkey, f.o.o_totalprice as o_totalprice from (select c_custkey, c_name, flatten(c_orders) as o from customer) f WHERE f.o.O_TOTALPRICE in (10,20,30,40,100,89230.03,270087.44);
---
+++ framework/resources/Functional/lateralUnnest/sf0dot01/07.sql
@@ -0,0 +1,1 @@
+select f.c_custkey, f.c_name, f.o.o_orderkey as o_orderkey, f.o.O_ORDERPRIORITY as O_ORDERPRIORITY from (select c_custkey, c_name, flatten(c_orders) as o from customer) f WHERE f.c_mktsegment like '%AUTOMOBILE%';
