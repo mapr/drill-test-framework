a5f9d8cfe5fb7380b1678d5e014ed0eb0fbe6de6
Abhishek Girish <agirish@apache.org>
2018-06-14 14:45:58 -0700

    Update before-run.sql

--- before-run.sql
+++ before-run.sql
@@ -1,5 +1,3 @@
-select * from sys.version;
-select * from sys.drillbits;
 alter system reset all;
 alter system set `planner.enable_decimal_data_type` = true;
 alter system set `new_view_default_permissions` = '777';
@@ -7,3 +5,7 @@ alter system set `planner.enable_limit0_optimization` = true;
 alter system set `exec.errors.verbose` = true;
 alter system set `planner.memory.max_query_memory_per_node` = 10737418240;
 alter system set `drill.exec.hashagg.fallback.enabled` = true;
+alter system set `drill.exec.hashjoin.fallback.enabled` = true;
+select * from sys.options where status = 'CHANGED';
+select * from sys.version;
+select * from sys.drillbits;
