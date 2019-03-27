9e462eb58cc13c838022f2146dde7151d658e5cd
Denys Ordynskiy <denysord88@gmail.com>
2019-03-27 17:51:54 +0200

    MD-5043 Added minio server to test S3 API in the drill-test-framework (#554)

---
+++ framework/resources/Datasources/s3/minio/run_mn.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+source conf/drillTestConfig.properties
+
+export MINIO_ACCESS_KEY=ZYVR33VM12IAHETIJYF9
+export MINIO_SECRET_KEY=BjGb9WpNd74t5T7FcBlXRgUcSyq505G598T8Yxg6
+
+if [ -z "$(top -bn1 |grep minio |grep -oE '^[ ]*[0-9]+' |grep -oE '[0-9]+')" ]
+then
+  nohup ${DRILL_TEST_DATA_DIR}/Datasources/s3/minio/minio server ${DRILL_TEST_DATA_DIR}/Datasources/s3/minio/minio_data >> ~/nohup.out &
+  sleep 1
+fi
