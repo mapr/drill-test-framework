a4264101019637e30ba79a5eb445f0e95cc9e20c
Pavel Semenov <semenovpavel1987@gmail.com>
2019-05-16 11:31:22 +0300

    DRILL-6974: Three new tests for 'set' command

---
+++ framework/resources/Functional/query_parser/DRILL-6974_alter_session_set_command_works.sql
@@ -0,0 +1,3 @@
+set  store.kafka.poll.timeout = 201;
+alter session set store.kafka.poll.timeout;
+reset store.kafka.poll.timeout;
---
+++ framework/resources/Functional/query_parser/DRILL-6974_set_property_can_be_updated.sql
@@ -0,0 +1,3 @@
+set exec.enable_union_type = true;
+set exec.enable_union_type;
+reset exec.enable_union_type;
