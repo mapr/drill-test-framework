3b0582ab0de19a601992536ba37e69ca2c96e95e
Robert Hou <robert.hou@hpe.com>
2021-03-08 02:15:26 -0800

    Add tests for MapR DB JSON.

---
+++ framework/resources/Datasources/maprdbjson/setup.sh
@@ -0,0 +1,19 @@
+set -x
+
+source conf/drillTestConfig.properties
+
+cd ${DRILL_TEST_DATA_DIR}/Datasources/maprdbjson echo `pwd`
+
+kubectl cp grades.json dataplatform/admincli-0:/tmp
+kubectl cp scores.json dataplatform/admincli-0:/tmp
+kubectl cp values.json dataplatform/admincli-0:/tmp
+kubectl cp valuesm1.json dataplatform/admincli-0:/tmp
+# kubectl cp valuesm1e.json dataplatform/admincli-0:/tmp
+kubectl cp load_data dataplatform/admincli-0:/tmp
+kubectl exec -i admincli-0 -n dataplatform -- /bin/bash -c "/tmp/load_data"
+# kubectl cp load_data_jsonapps dataplatform/admincli-0:/tmp
+# kubectl cp hbase.replicate1 dataplatform/admincli-0:/tmp
+# kubectl cp hbase.replicate2 dataplatform/admincli-0:/tmp
+# kubectl cp hbase.replicate3 dataplatform/admincli-0:/tmp
+# kubectl cp hbase.replicate4 dataplatform/admincli-0:/tmp
+# kubectl exec -i admincli-0 -n dataplatform -- /bin/bash -c "/tmp/load_data_jsonapps"
---
+++ framework/resources/Functional/maprdbjson/delete1.sh
@@ -0,0 +1,3 @@
+cd framework/resources/Functional/maprdbjson
+kubectl cp delete1 dataplatform/admincli-0:/tmp
+kubectl exec -i admincli-0 -n dataplatform -- /bin/bash /tmp/delete1
