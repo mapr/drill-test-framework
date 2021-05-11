403eb164ad3f2d6faa8b6902c72c9580abbf2c0d
Robert Hou <rhou@maprtech.com>
2021-05-10 21:16:37 -0700

    Add some volume mirror tests. (#647)
    Co-authored-by: Robert Hou <robert.hou@hpe.com>

---
+++ framework/resources/Functional/mirror/mirror.sh
@@ -0,0 +1,15 @@
+set -x
+
+cd framework/resources/Functional/volume
+
+kubectl cp mirror dataplatform/cldb-0:/tmp
+kubectl cp hbase.replicate1 dataplatform/cldb-0:/tmp
+kubectl cp hbase.replicate2 dataplatform/cldb-0:/tmp
+kubectl cp hbase.replicate3 dataplatform/cldb-0:/tmp
+kubectl cp hbase.replicate5 dataplatform/cldb-0:/tmp
+kubectl cp hbase.replicate6 dataplatform/cldb-0:/tmp
+kubectl cp hbase.replicate7 dataplatform/cldb-0:/tmp
+kubectl cp hbase.replicate8 dataplatform/cldb-0:/tmp
+kubectl cp hbase.replicate9 dataplatform/cldb-0:/tmp
+kubectl exec -i cldb-0 -n dataplatform -- /bin/bash -c "/tmp/mirror"
+
