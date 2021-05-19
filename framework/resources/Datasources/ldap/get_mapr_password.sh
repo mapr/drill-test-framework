fa237c21c411b9a74f757c23860c37e5483a20a3
Robert Hou <robert.hou@hpe.com>
2021-05-19 15:19:53 -0700

    Update ldap tests
    Update ldap tests to get new mapr password.

--- framework/resources/Datasources/ldap/get_mapr_password.sh
+++ framework/resources/Datasources/ldap/get_mapr_password.sh
@@ -1,3 +1,4 @@
 set -x
 
-kubectl get secret system-user-secrets -n hpe-secure -o yaml | grep MAPR_PASSWORD | head -1 | awk '{print $2}' | base64 --decode
+# kubectl get secret system-user-secrets -n hpe-secure -o yaml | grep MAPR_PASSWORD | head -1 | awk '{print $2}' | base64 --decode
+kubectl get secret system -n dataplatform -o yaml | grep MAPR_PASSWORD | head -1 | awk '{print $2}' | base64 --decode
