dede3e8b76b4366441ac6f668743e5dd2ad8a330
demyd <demyd@dlaptop.bb8core.lab>
2021-08-04 17:11:27 +0300

    do skip sanity testSSLEnabled if unsecure cluster

--- framework/src/test/java/org/apache/drill/test/framework/TestSSLProperties.java
+++ framework/src/test/java/org/apache/drill/test/framework/TestSSLProperties.java
@@ -11,48 +11,50 @@ import static org.junit.Assert.fail;
 
 public class TestSSLProperties {
 
-  @Test
-  public void testSSLDisabled() throws Exception {
-    Properties connectionProperties = new Properties();
+    @Test
+    public void testSSLDisabled() throws Exception {
+        Properties connectionProperties = new Properties();
 
-    connectionProperties.put("auth", DrillTestDefaults.AUTHENTICATION_MECHANISM);
-    connectionProperties.put("enableTLS", "false");
-    connectionProperties.put("user", DrillTestDefaults.USERNAME);
-    connectionProperties.put("password", DrillTestDefaults.PASSWORD);
+        connectionProperties.put("auth", DrillTestDefaults.AUTHENTICATION_MECHANISM);
+        connectionProperties.put("enableTLS", "false");
+        connectionProperties.put("user", DrillTestDefaults.USERNAME);
+        connectionProperties.put("password", DrillTestDefaults.PASSWORD);
 
-    ConnectionPool connectionPool = new ConnectionPool(connectionProperties);
-    try {
-      connectionPool.createConnection(connectionProperties);
-    } catch (SQLException ex) {
-      String message = "Error setting/closing connection. Details: HANDSHAKE_COMMUNICATION";
-      assertTrue(ex.getMessage().contains(message));
+        ConnectionPool connectionPool = new ConnectionPool(connectionProperties);
+        try {
+            connectionPool.createConnection(connectionProperties);
+        } catch (SQLException ex) {
+            String message = "Error setting/closing connection. Details: HANDSHAKE_COMMUNICATION";
+            assertTrue(ex.getMessage().contains(message));
+        }
     }
-  }
 
-  @Test
-  public void testSSLEnabled() throws Exception {
-    Properties connectionProperties = new Properties();
+    @Test
+    public void testSSLEnabled() throws Exception {
+        if (DrillTestDefaults.TRUSTSTORE_PATH != null && DrillTestDefaults.TRUSTSTORE_PATH.length() != 0) {
+            Properties connectionProperties = new Properties();
 
-    connectionProperties.put("auth", DrillTestDefaults.AUTHENTICATION_MECHANISM);
-    connectionProperties.put("enableTLS", "true");
-    connectionProperties.put("trustStorePath", DrillTestDefaults.TRUSTSTORE_PATH);
-    connectionProperties.put("trustStorePassword", DrillTestDefaults.TRUSTSTORE_PASSWORD);
-    connectionProperties.put("user", DrillTestDefaults.USERNAME);
-    connectionProperties.put("password", DrillTestDefaults.PASSWORD);
+            connectionProperties.put("auth", DrillTestDefaults.AUTHENTICATION_MECHANISM);
+            connectionProperties.put("enableTLS", "true");
+            connectionProperties.put("trustStorePath", DrillTestDefaults.TRUSTSTORE_PATH);
+            connectionProperties.put("trustStorePassword", DrillTestDefaults.TRUSTSTORE_PASSWORD);
+            connectionProperties.put("user", DrillTestDefaults.USERNAME);
+            connectionProperties.put("password", DrillTestDefaults.PASSWORD);
 
-    ConnectionPool connectionPool = new ConnectionPool(connectionProperties);
-    try {
-      connectionPool.createConnection(connectionProperties);
-    } catch (SQLException ex) {
-      ex.printStackTrace();
-      fail("Establishing connection failed.");
-    } finally {
-      Connection connection = connectionPool.createConnection(connectionProperties);
-      assertTrue(Utils.sanityTest(connection));
-      connection.close();
-      connectionPool.close();
+            ConnectionPool connectionPool = new ConnectionPool(connectionProperties);
+            try {
+                connectionPool.createConnection(connectionProperties);
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+                fail("Establishing connection failed.");
+            } finally {
+                Connection connection = connectionPool.createConnection(connectionProperties);
+                assertTrue(Utils.sanityTest(connection));
+                connection.close();
+                connectionPool.close();
+            }
+        }
     }
-  }
 
 }
 
