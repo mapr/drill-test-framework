51913c16924a6b9abe883cb61004737adbc61cc9
Demyd <dhoncharuk@mapr.com>
2021-11-23 00:54:12 +0200

    Fixed jpam on mapr-cluster security (#679)
    Co-authored-by: demyd.honcharuk <demyd.honcharuk@hpe.com>

--- framework/src/main/java/org/apache/drill/test/framework/Utils.java
+++ framework/src/main/java/org/apache/drill/test/framework/Utils.java
@@ -1,19 +1,19 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/*
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
  */
 package org.apache.drill.test.framework;
 
@@ -92,22 +92,24 @@ public class Utils {
   public static Properties createConnectionProperties() {
     Properties connectionProperties = new Properties();
 
-    if (DrillTestDefaults.AUTHENTICATION_MECHANISM.equals("PLAIN")) {
-      connectionProperties.put("auth", "PLAIN");
-    } else if (DrillTestDefaults.AUTHENTICATION_MECHANISM.equals("MAPRSASL")) {
-      connectionProperties.put("auth", "MAPRSASL");
-    } else if (DrillTestDefaults.AUTHENTICATION_MECHANISM.equals("KERBEROS")) {
-      connectionProperties.put("auth", "KERBEROS");
-      connectionProperties.put("principal", DrillTestDefaults.KERBEROS_PRINCIPAL);
+    switch (DrillTestDefaults.AUTHENTICATION_MECHANISM) {
+      case "PLAIN":
+        connectionProperties.put("auth", "PLAIN");
+        if (DrillTestDefaults.SSL_ENABLED) {
+          connectionProperties.put("enableTLS", "true");
+          connectionProperties.put("disableHostVerification", "true");
+          connectionProperties.put("trustStorePath", DrillTestDefaults.TRUSTSTORE_PATH);
+          connectionProperties.put("trustStorePassword", DrillTestDefaults.TRUSTSTORE_PASSWORD);
+        }
+        break;
+      case "MAPRSASL":
+        connectionProperties.put("auth", "MAPRSASL");
+        break;
+      case "KERBEROS":
+        connectionProperties.put("auth", "KERBEROS");
+        connectionProperties.put("principal", DrillTestDefaults.KERBEROS_PRINCIPAL);
+        break;
     }
-
-    if (DrillTestDefaults.SSL_ENABLED) {
-      connectionProperties.put("enableTLS", "true");
-      connectionProperties.put("disableHostVerification", "true");
-      connectionProperties.put("trustStorePath", DrillTestDefaults.TRUSTSTORE_PATH);
-      connectionProperties.put("trustStorePassword", DrillTestDefaults.TRUSTSTORE_PASSWORD);
-    }
-
     return connectionProperties;
   }
 
@@ -146,7 +148,7 @@ public class Utils {
     sqlNullabilities = ImmutableMap.copyOf(nullabilityMap);
 
     client = getHTTPClientInstance();
-    if (DrillTestDefaults.AUTHENTICATION_MECHANISM.equals("PLAIN")) {
+    if (DrillTestDefaults.IS_SECURE_CLUSTER) {
       authenticateHTTPClient();
     }
   }
