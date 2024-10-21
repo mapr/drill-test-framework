761999de64d1e4000fa25388cdbc7b0e8f2bcaaa
Konstantin Harasov <konstantin.harasov@hpe.com>
2024-10-21 12:16:06 +0300

    fix for Functional/joins tests for creating tmp table

--- framework/resources/Functional/joins/inner/DRILL-6612_temp_with_pers_table_join.sql
+++ framework/resources/Functional/joins/inner/DRILL-6612_temp_with_pers_table_join.sql
@@ -3,4 +3,4 @@ create TEMPORARY table temp_tab as select '12312' as c2;
 drop table if exists dfs.drillTestDir.pers_tab;
 create table dfs.drillTestDir.pers_tab as select '12312' as c1;
 --@test
-select * from dfs.drillTestDir.`pers_tab` a join `temp_tab` b on a.c1 = b.c2;
+select * from dfs.drillTestDir.`pers_tab` a join dfs.tmp.temp_tab b on a.c1 = b.c2;
